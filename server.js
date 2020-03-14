5dc1183a051e6cfd49889bea0e415f45ce99f28b
Adalberto Taylor <adalbertotaylor84@gmail.com>
2020-03-14 18:06:23 -0300

    Clean server.js

--- server.js
+++ server.js
@@ -1,36 +1,12 @@
-'use strict';
+const express = require('express')
+const bodyParser = require('body-parser')
 
-//mongoose file must be loaded before all other files in order to provide
-// models to other modules
-var express = require('express'),
-  router = express.Router(),
-  bodyParser = require('body-parser')
+const { router: userRouter } = require('./src/routers/user')
+const { router: swaggerRouter} = require('./src/routers/swagger')
 
 const PORT = process.env.PORT || 3000
-  
-  
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json');
 
-var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
-
-// mongoose.connect('mongodb://localhost:27017/swagger-demo');
-
-var UserSchema = new Schema({
-  email: {
-    type: String, required: true,
-    trim: true, unique: true,
-    match: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
-  },
-  firstName: {type: String},
-  lastName: {type: String}
-});
-
-mongoose.model('User', UserSchema);
-var User = require('mongoose').model('User');
-
-var app = express();
+const app = express();
 
 //rest API requirements
 app.use(bodyParser.urlencoded({
@@ -38,77 +14,8 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-//middleware for create
-var createUser = function (req, res, next) {
-  var user = new User(req.body);
-
-  user.save(function (err) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(user);
-    }
-  });
-};
-
-var updateUser = function (req, res, next) {
-  User.findByIdAndUpdate(req.body._id, req.body, {new: true}, function (err, user) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(user);
-    }
-  });
-};
-
-var deleteUser = function (req, res, next) {
-  req.user.remove(function (err) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(req.user);
-    }
-  });
-};
-
-var getAllUsers = function (req, res, next) {
-  User.find(function (err, users) {
-    if (err) {
-      next(err);
-    } else {
-      res.json(users);
-    }
-  });
-};
-
-var getOneUser = function (req, res) {
-  res.json(req.user);
-};
-
-var getByIdUser = function (req, res, next, id) {
-  User.findOne({_id: id}, function (err, user) {
-    if (err) {
-      next(err);
-    } else {
-      req.user = user;
-      next();
-    }
-  });
-};
-
-router.route('/users')
-  .post(createUser)
-  .get(getAllUsers);
-
-router.route('/users/:userId')
-  .get(getOneUser)
-  .put(updateUser)
-  .delete(deleteUser);
-
-router.param('userId', getByIdUser);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use('/api/v1', router);
+app.use(swaggerRouter);
+app.use('/api/v1', userRouter);
 
 app.listen(PORT, () => console.log('Server ready...'));
 module.exports = app;
