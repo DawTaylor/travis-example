ea77338c71296d4c4d17fab4fa5a93122d8c9c8b
Adalberto Taylor <adalbertotaylor84@gmail.com>
2020-03-14 18:10:13 -0300

    Remove static property and make inner DB private

--- src/models/User.js
+++ src/models/User.js
@@ -2,11 +2,12 @@ const { Types: { ObjectId }} = require('mongoose')
 const R = require('ramda')
 const { db: DB } = require('../database/Database')
 
+const COLLECTION = 'users'
+
 class User {
-  static collection = 'users'
+  #db
   constructor(data) {
-    this.db = DB
-    this.collection = 'users'
+    this.#db = DB
     const newId = new ObjectId().toString()
     this.user = R.pipe(
       R.pickAll(['email', 'firstName', 'lastName']),
@@ -15,36 +16,36 @@ class User {
   }
 
   static findOne(query, fn) {
-    const user = R.head(DB.getEntities(query, this.collection))
+    const user = R.head(DB.getEntities(query, COLLECTION))
     if(!user || user.length < 1) return (fn('user not found'))
     return fn(null, user)
   }
 
   static findByIdAndUpdate(id, payload, _, fn) {
-    const updatedUser = DB.updateEntity(id, payload, this.collection)
+    const updatedUser = DB.updateEntity(id, payload, COLLECTION)
 
     return fn(null, updatedUser)
   }
 
   static find(fn) {
-    const users = DB.getEntities(null, this.collection)
+    const users = DB.getEntities(null, COLLECTION)
     return fn(null, users)
   }
 
   static remove(id, fn) {
-    DB.deleteEntity(id, this.collection)
+    DB.deleteEntity(id, COLLECTION)
 
     return fn()
   }
 
   save(fn) {
-    const existingUser = this.db.getEntities({ email: this.user.email }, this.collection)
+    const existingUser = this.#db.getEntities({ email: this.user.email }, COLLECTION)
 
     if(existingUser && existingUser.length > 0) {
       return fn('user.email should be unique')
     }
 
-    this.db.saveEntity(this.user, this.collection)
+    this.#db.saveEntity(this.user, COLLECTION)
 
     fn()
   }
